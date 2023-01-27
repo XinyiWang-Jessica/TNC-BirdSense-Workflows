b9aa1fcf6da648a8a7b1535c7c2a6c1f1d71f3dc
Xinyi (Jessica) Wang <108918930+XinyiWang-Jessica@users.noreply.github.com>
2023-01-27 12:47:32 -0800

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -11,16 +11,6 @@ program = "WB4B22"
 thresh_val = 0.25
 cloud_free_thresh = 0.5
 
-
-in_fields_W21 = ee.FeatureCollection("users/kklausmeyer/Bid4Birds_Fields_Winter2021_1206")
-in_fields_F21 = ee.FeatureCollection("users/kklausmeyer/B4B_fields_Fall2021");
-in_fields_WDW21 = ee.FeatureCollection("users/kklausmeyer/BR_21_WDW");
-in_fields_WDF21 = ee.FeatureCollection("users/kklausmeyer/BR_21_WDF_enrolled");
-in_fields_WB4B22 = ee.FeatureCollection("projects/codefornature/assets/B4B_fields_Winter2022");
-in_fields_WCWR22 = ee.FeatureCollection("projects/codefornature/assets/CWRHIP_fields_Winter2022");
-in_fields_WSOD22 = ee.FeatureCollection("projects/codefornature/assets/DSOD_fields_Winter2022");
-in_fields_WDDR22 = ee.FeatureCollection("projects/codefornature/assets/DDR_fields_Winter2022");
-
 if program == "W21":
   fields = in_fields_W21
   bid_name = 'Bid_ID'
