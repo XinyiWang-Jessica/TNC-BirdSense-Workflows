2915541884dc13275a4f6c5cef107541082c7ec2
Xinyi (Jessica) Wang <108918930+XinyiWang-Jessica@users.noreply.github.com>
2023-01-29 23:57:13 -0800

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -11,38 +11,38 @@ program = "WB4B22"
 thresh_val = 0.25
 cloud_free_thresh = 0.5
 
-if program == "W21":
-  fields = in_fields_W21
-  bid_name = 'Bid_ID'
-  field_name = 'Field_ID'
-elif program == "F21":
-  fields = in_fields_F21
-  bid_name = 'Bid_ID'
-  field_name = 'Field_ID'
-elif program == "WDW21":
-  fields = in_fields_WDW21
-  bid_name = 'wn21_ID'
-  field_name = 'Field_Name'
-elif program == "WDF21":
-  fields = in_fields_WDF21
-  bid_name = 'wn21_ID'
-  field_name = 'Field_Name'
-elif program == "WB4B22":
-  fields = in_fields_WB4B22
-  bid_name = 'BidID'
-  field_name = 'FieldID'
-elif program == "WCWR22":
-  fields = in_fields_WCWR22
-  bid_name = 'Contract_I'
-  field_name = 'Field_Name'
-elif program == "WSOD22":
-  fields = in_fields_WSOD22
-  bid_name = 'BidID'
-  field_name = 'FieldID'
-elif program == "WDDR22":
-  fields = in_fields_WDDR22
-  bid_name = 'BidID'
-  field_name = 'FieldID'
+# if program == "W21":
+#   fields = in_fields_W21
+#   bid_name = 'Bid_ID'
+#   field_name = 'Field_ID'
+# elif program == "F21":
+#   fields = in_fields_F21
+#   bid_name = 'Bid_ID'
+#   field_name = 'Field_ID'
+# elif program == "WDW21":
+#   fields = in_fields_WDW21
+#   bid_name = 'wn21_ID'
+#   field_name = 'Field_Name'
+# elif program == "WDF21":
+#   fields = in_fields_WDF21
+#   bid_name = 'wn21_ID'
+#   field_name = 'Field_Name'
+# elif program == "WB4B22":
+#   fields = in_fields_WB4B22
+#   bid_name = 'BidID'
+#   field_name = 'FieldID'
+# elif program == "WCWR22":
+#   fields = in_fields_WCWR22
+#   bid_name = 'Contract_I'
+#   field_name = 'Field_Name'
+# elif program == "WSOD22":
+#   fields = in_fields_WSOD22
+#   bid_name = 'BidID'
+#   field_name = 'FieldID'
+# elif program == "WDDR22":
+#   fields = in_fields_WDDR22
+#   bid_name = 'BidID'
+#   field_name = 'FieldID'
 
 s2_vis_params = {
     'bands': ['B4', 'B3', 'B2'],
