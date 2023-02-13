d17add11242cafac29c926c41174dff14b904ca9
Elena Liao <b03607061@gmail.com>
2023-02-13 15:04:08 -0800

    flood status number change v2

--- step3.py
+++ step3.py
@@ -239,8 +239,8 @@ def history_plot(df, start, n=8):
 #     plt.yticks([])
 #     return fig
 
-def plot_status(df, start, start_last):
-    num, percent, percent2 = cloud_free_percent(df, start_last)
+def plot_status(df, start):
+    num, percent, percent2 = cloud_free_percent(df, start)
     start_last = dt.datetime.strptime(start, '%Y-%m-%d').date()
     start_last2 = (start_last - dt.timedelta(days=7)).strftime('%Y-%m-%d')
     bin_labels = ['Minimally Flooded', 'Partially Flooded', 'Flooded']
