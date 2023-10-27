d50cba2f3ebea0955c10db73a76143388bac36b2
XinyiWang-Jessica <108918930+XinyiWang-Jessica@users.noreply.github.com>
2023-10-27 13:08:28 -0700

    update step 3

--- step3.py
+++ step3.py
@@ -46,7 +46,7 @@ def create_report(df_daily, df_pivot, watch, program, fields, start_string, col)
     page = dp.Page(
         title = program_name, 
         blocks = [
-            dp.Text('# BirdSense #'),
+            dp.Text('# BirdSense - test with Dynamic World #'),
             dp.Text(f'## Program - {program_name} ##'),
             dp.Text(f'## {season} ##'),
             dp.Text(f'### Weekly Report - {start_last_text} to {end_last_text} ###'),
@@ -132,33 +132,42 @@ def all_heatmaps(df, col, start):
     return heatmaps, cut_bin
 
 
-def history_plot(df, start, n=8, cloudy = 0.1):
+def history_plot(df, start_last_week, start_string, max_bar=8, cloudy = 0.1):
     '''
-    plot the last n weeks data with plotly`
+    plot the last n weeks data with plotly
     '''
+    
     columns = df.columns.tolist()
-    start_last = dt.datetime.strptime(start, '%Y-%m-%d').date()
-    # print(columns)
+    # calculate the number of full weeks n
+    start_last = dt.datetime.strptime(start_last_week, '%Y-%m-%d').date()
+    start_program = dt.datetime.strptime(start_string, '%Y-%m-%d').date()
+    diff = start_last - start_program
+    n = min(ceil((diff.days + 6)/7), max_bar)
     last = [' ']
+    
+    # if the last week is not a full week, remove it
     if dt.datetime.strptime(columns[-1], '%Y-%m-%d').date() > start_last:
-        columns = columns[:-1]
-    columns = columns[-n:]
-    all_columns = columns.copy()
+        columns = columns[:-1] 
+    
+    week_columns = columns[-n:].copy()
+    # all_columns = columns.copy()
     no_columns = []
-    for column in columns:
+    
+    # hide the weeks with high cloudy percentage
+    for column in week_columns:
         if df[column].count() < len(df)*cloudy:
-            columns.remove(column)
+            week_columns.remove(column)
             no_columns.append(column)
-    # last_n_week_all = df[columns].applymap(
-    #     lambda x: 1 if x >= 0 else 0).sum()/df[columns].count()
-    # print(columns)
-    last_n_week = df[columns].applymap(
-        lambda x: 1 if x > 0.66 else 0).sum()/df[columns].count()
-    last_n_week_par = df[columns].applymap(
-        lambda x: 1 if x > 0.33 and x <= 0.66 else 0).sum()/df[columns].count()
-    last_n_week_non = df[columns].applymap(
-        lambda x: 1 if x <= 0.33 else 0).sum()/df[columns].count()
-
+    print(week_columns, n)        
+    # calculate the percentage of minimal, partial and flooded for each week
+    last_n_week = df[week_columns].applymap(
+        lambda x: 1 if x > 0.66 else 0).sum()/df[week_columns].count()
+    last_n_week_par = df[week_columns].applymap(
+        lambda x: 1 if x > 0.33 and x <= 0.66 else 0).sum()/df[week_columns].count()
+    last_n_week_non = df[week_columns].applymap(
+        lambda x: 1 if x <= 0.33 else 0).sum()/df[week_columns].count()
+    
+    # plot the historical flooding status
     fig = go.Figure(data=[
         go.Bar(name='Flooded', x=last_n_week.index,
                y=last_n_week.round(3), marker_color='#063970',
@@ -199,26 +208,27 @@ def history_plot(df, start, n=8, cloudy = 0.1):
                       xaxis = dict(
                           title='Start of the Weeks',
                           tickmode = 'array',
-                          tickvals = all_columns),
+                          tickvals = week_columns),
                       title = {'text': 'Flooding Status for Last 8 Weeks (Cloud-Free Fields Only)',
                               'x': 0.5,'y': 0.9,
                               'xanchor': 'center',
                              'font': {'size': 16}},
                         annotations=[
                             dict(
-                                x=columns[2],
-                                # xanchor='center',
+                                x=week_columns[0],
+                                xanchor='left',
                                 y=1.2,
                                 text="* Weeks with satellite data availability less than {:.0%} are excluded".format(cloudy),
                                 showarrow=False
                                  )
                                      ]
                      )
-    all_columns = all_columns + last
+
+    all_columns = week_columns + last
+
     fig.update_yaxes(showticklabels=False, range=[0, 1.3])
     fig.update_xaxes(range = [all_columns[0], all_columns[-1]])
     return fig
-
 
 def plot_status(df, start):
     '''
@@ -227,9 +237,10 @@ def plot_status(df, start):
     and compare with last week
     '''
     start_last = dt.datetime.strptime(start, '%Y-%m-%d').date()
+    start_text = start_last.strftime("%b %d, %Y")
     start_last2 = (start_last - dt.timedelta(days=7)).strftime('%Y-%m-%d')
     bin_labels = ['Minimally Flooded', 'Partially Flooded', 'Flooded']
-    cnt = df.count()
+    # cnt = df.count()
     level = pd.cut(df[start],
                    bins=[-1, .33, .66, 1],
                    labels=bin_labels)
@@ -280,7 +291,7 @@ def plot_status(df, start):
         font=dict(
             size=20
         ),
-        title = {'text': f'Flooding Status for the Week of {start} (Cloud-Free Fields Only)',
+        title = {'text': f'Flooding Status for the Week of {start_text} (Cloud-Free Fields Only)',
                               'x': 0.5,'y': 0.9,
                               'xanchor': 'center',
                              'font': {'size': 16}}
@@ -292,6 +303,7 @@ def plot_cloudy_status(start, cloudy = 0.15):
     '''
     if the cloud free fields percentage is below the threshold, hide the status plot
     '''
+    start_text = dt.datetime.strptime(start, '%Y-%m-%d').strftime("%b %d, %Y")
     fig = go.Figure()
     fig.add_annotation(text = "* Status is not shown if the satellite data availability is less than {:.0%}.".format(cloudy),
                     #    align ='left',
@@ -307,7 +319,7 @@ def plot_cloudy_status(start, cloudy = 0.15):
         autosize=True,
         font=dict(size=20),
         plot_bgcolor="rgba(0,0,0,0)",
-        title = {'text': f'Flooding Status for the Week of {start} (Cloud-Free Fields Only)',
+        title = {'text': f'Flooding Status for the Week of {start_text} (Cloud-Free Fields Only)',
                               'x': 0.5,'y': 0.9,
                               'xanchor': 'center',
                              'font': {'size': 16}}
@@ -323,20 +335,31 @@ def map_plot(fields, df, program, start):
     the flooding percentage results from last week,
     and plot a map
     '''
+    start_text = dt.datetime.strptime(start, '%Y-%m-%d').strftime("%b %d, %Y")
     # obtain geometry information and convert to dataframe
     df_geo = gpd.read_file(json.dumps(fields.getInfo()))
     
-    # change name to satanderd name
+    # Change name to satanderd name
     df_geo = standardize_names(df_geo, 'Bid_ID', field_bid_names[program][0])
     df_geo = standardize_names(df_geo, 'Field_ID', field_bid_names[program][1])
-    # join the flooding percentage results to the geo dataframe
+    # Join the flooding percentage results to the geo dataframe
     merged_df = pd.merge(df_geo, df, 
                          left_on=['Bid_ID','Field_ID'], 
                          right_on= list(df.columns[:2]))
     merged_df['Flooding %'] = merged_df[start].round(3)*100
     merged_df['week'] = f'Week starting on {start}'
     
-    #    # check if the input dataframe include flooding start and end tats
+    # Look for the center and zoom of the map presentation
+    box_boundary = merged_df['geometry'].bounds
+    lon_min = box_boundary['minx'].min()
+    lon_max = box_boundary['maxx'].max()
+    lat_min = box_boundary['miny'].min()
+    lat_max = box_boundary['maxy'].max()
+    center = dict(lat = (lat_min + lat_max) / 2, lon = (lon_min + lon_max) / 2)
+    max_bound = max(abs(lon_min-lon_max), abs(lat_min-lat_max)) * 111
+    zoom = 12.8 - np.log(max_bound)
+    
+    # Check if the input dataframe include flooding start and end tats
     if 'Flood_start' in merged_df.columns:
         columns = ['Bid_ID', 'Field_ID', 'Flood Start', 'Flood End']
         hover='Bid_ID: %{customdata[0]}<br>' + \
@@ -347,7 +370,7 @@ def map_plot(fields, df, program, start):
         columns = ['Bid_ID', 'Field_ID']
         hover='Bid_ID: %{customdata[0]}<br>' + 'Field_ID: %{customdata[1]}<br>' 
     
-    # map plot
+    # Map plot
     fig = px.choropleth_mapbox(
         data_frame = merged_df,            # Data frame with values
         geojson = fields.getInfo(),                      # Geojson with geometries
@@ -356,12 +379,13 @@ def map_plot(fields, df, program, start):
         hover_name = 'week', 
         hover_data = columns,
         color = 'Flooding %',            # Name of the column of the data frame with the data to be represented
-        mapbox_style = 'stamen-terrain',
+        mapbox_style = 'open-street-map',
         color_continuous_scale = 'RdBu',
         opacity = 0.7,
-        center = dict(lat = 39.141, lon = -121.63),
-        zoom = 8)
-    # add the fieds without data to the map
+        center = center,
+        zoom = zoom
+    )
+    # Add the fieds without data to the map
     no_data_fields = go.Choroplethmapbox(
         geojson=fields.getInfo(),
         locations=merged_df[merged_df['Flooding %'].isna()]['id'],
@@ -373,10 +397,123 @@ def map_plot(fields, df, program, start):
         hovertemplate=hover
     )
     fig.add_trace(no_data_fields)
-    fig.update_layout(height=800, width = 1000, 
-                  autosize = True,
-                  title = {'text': f'Flooding Status for the week starting on {start}',
-                              'x': 0.5,'y': 0.95,
-                              'xanchor': 'center',
-                             'font': {'size': 16}})
+    fig.update_layout(
+        height=800, 
+        width = 1000, 
+        autosize = True,
+        title = {'text': f'Flooding Status for the week starting on {start_text}',
+                 'x': 0.5,'y': 0.95,
+                 'xanchor': 'center',
+                 'font': {'size': 16}
+                 }
+    )
+    return fig
+
+
+def history_plot_by_id(df_daily, df_weekly):
+    '''
+    This function takes the daily flooding status table
+    return a interactive plot with filter function by bid_id
+    '''
+    # data preparation
+    df_daily_clean = df_daily.groupby(['Unique_ID', 'Date'])\
+                            .agg({'pct_flood': 'mean', 'Field_ID' : 'first'}).reset_index()
+    df_daily_clean = df_daily_clean.dropna()
+    # create Unique_ID for table merge and formate the flood start/end dates to datetime type
+    df_weekly['Unique_ID'] = df_weekly['Bid_ID'] + '_' + df_weekly['Field_ID']
+    df_weekly['Flood Start'] = pd.to_datetime(df_weekly['Flood Start'])
+    df_weekly['Flood End'] = pd.to_datetime(df_weekly['Flood End'])
+    # merge the flood start and end dates from weekly table to daily table
+    df_daily_clean = pd.merge(df_daily_clean, 
+                              df_weekly[['Unique_ID', 'Flood Start', 'Flood End']], 
+                              on = ['Unique_ID'], how = 'left')
+    df_daily_clean['FieldID_date'] = df_daily_clean['Field_ID'] + ': ' \
+                                + df_daily_clean['Flood Start'].apply(lambda x : x.strftime("%b %d")) + ' to ' \
+                                + df_daily_clean['Flood End'].apply(lambda x : x.strftime("%b %d, %Y"))
+    
+    bid_ids = sorted(df_daily.dropna()['Bid_ID'].unique())
+    unique_ids = df_daily_clean['Unique_ID'].unique()
+    # build a menu for each Bid_ID
+    # add a choice of all the fields   
+    menus = [dict(
+        label = "All",
+        method = "update",
+        args = [{"visible": [True] * len(unique_ids) + [False] * len(unique_ids)},
+               {"title": "Flooding status history for all fields"}
+                ])]
+
+    # add one choice for each Bid_ID
+    for bid_id in bid_ids:
+        visibility = [True if bid_id in unique_id else False for unique_id in unique_ids] \
+                    + [True if bid_id == select_id else False for select_id in bid_ids]
+        # print(visibility)
+        menus.append(dict(
+            label = bid_id,
+            method = "update",
+            args = [{"visible": visibility},
+                    {"title": f"Flooding time series for {bid_id}"}
+                   ]))
+        
+    # Initialize figure
+    fig = go.Figure()
+    
+    # Add Trace for each unique id
+    for unique_id in unique_ids:
+        # filter data for the selected unique_id
+        df_select = df_daily_clean[df_daily_clean['Unique_ID'] == unique_id]
+        
+        fig.add_trace(
+            go.Scatter(x = df_select.Date,
+                       y = df_select['pct_flood']*100,
+                       name = min(df_select.FieldID_date),
+                       # mode = 'lines'
+                  ))
+    
+    # Add shaded area indicating flooding period for each bid_id
+    for bid_id in bid_ids:
+        df_select = df_weekly[df_weekly['Bid_ID'] == bid_id].reset_index()
+        start = min(df_select['Flood Start'])
+        end = max(df_select['Flood End'])
+        fig.add_trace(
+            go.Scatter(x = [start, 
+                            end], 
+                       y = [100, 100],
+                       mode='lines',                # 'lines' mode to connect the points
+                       fill='tozeroy',              # 'tozeroy' fills below the curve
+                       fillcolor='rgba(0, 0, 255, 0.2)',  # Color and opacity of the shaded area
+                       name='Contracted Flooding Period',
+                       line_width = 0,
+                       visible = False,
+                       )
+        )
+        
+    # add dropdown menus and format the plot layout
+    fig.update_layout(
+        updatemenus = [
+            dict(
+                type = "dropdown",
+                direction = "down",
+                
+                buttons = menus,
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=-0.3,
+                xanchor="left",
+                y=1,
+                yanchor="top"
+            )],
+        annotations=[
+        dict(text="Select a Bid ID:", showarrow=False,
+        x= -0.3, y=1.1, xref="paper", yref="paper", align="left")
+        ],
+        yaxis={"title": 'Flooding %'},
+        plot_bgcolor='white',
+        showlegend = True,
+    )
+
+    # format plot
+    fig.update_yaxes(range=[-5, 101],
+                     gridcolor='lightgrey')
+    fig.update_xaxes(range=[min(df_daily.Date), dt.datetime.now().date()],
+                     )
     return fig
